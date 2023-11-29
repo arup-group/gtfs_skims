605ba595ef8f6e28d02deed08e65ee737c98a1fe
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2023-11-29 09:35:05 +0000

    Fix cookiecutter issues

--- docs/static/hooks.py
+++ docs/static/hooks.py
@@ -12,9 +12,10 @@ API_FILES_TO_IGNORE = ["cli.py"]
 # Bump priority to ensure files are moved before jupyter notebook conversion takes place
 @mkdocs.plugins.event_priority(50)
 def on_files(files: list, config: dict, **kwargs):
-    """Link (1) top-level files to mkdocs files and (2) generate the python API documentation."""for file in Path("./resources").glob("**/*.*"):
+    """Link (1) top-level files to mkdocs files and (2) generate the python API documentation."""
+    for file in Path("./resources").glob("**/*.*"):
         files.append(_new_file(file, config))
-    files.append(_new_file("./CHANGELOG.md", config))
+    files.append(_new_file(Path("./CHANGELOG.md"), config))
 
     api_nav = _api_gen(files, config)
     _update_nav(api_nav, config)
@@ -22,14 +23,14 @@ def on_files(files: list, config: dict, **kwargs):
     return files
 
 
-def _new_file(path: Path, config: str, src_dir: str = ".") -> File:
+def _new_file(path: Path, config: dict, src_dir: str = ".") -> File:
     """Link a file out in the wilderness to a filename in the documentation directory hierarchy.
 
     Args:
         path (Path):
             Path (relative to "src_dir") to file that you want to bring into the documentation directory.
             In the documentation directory, this same path with apply (e.g., `[src_dir]/path/to/file.md` will become `[docs_dir]/path/to/file.md`)
-        config (str): mkdocs config dictionary.
+        config (dict): mkdocs config dictionary.
         src_dir (str, optional):
             Path in which to find the file you want to bring into the docs directory. Defaults to ".".
 
