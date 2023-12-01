97785a3658b066771d439d4b46cdea5951cd7254
Theodore.Chatziioannou <Theodore.Chatziioannou@arup.com>
2023-12-01 11:26:54 +0000

    initial structure

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,14 @@ def test_content(response):
     assert response.content
 ```
 """
+import os
+from pathlib import Path
 
 import pytest
+
+from gtfs_skims.utils import Config, GTFSData
+
+TEST_DATA_DIR = os.path.join(Path(__file__).parent, 'test_data')
 
 
 @pytest.fixture
@@ -19,3 +25,12 @@ def response():
     """
     # import requests
     # return requests.get('https://github.com/arup-group/cookiecutter-pypackage')
+
+
+@pytest.fixture
+def config():
+    return Config.from_yaml(os.path.join(TEST_DATA_DIR, 'config_demo.yaml'))
+
+@pytest.fixture
+def gtfs_data():
+    return GTFSData.from_gtfs(os.path.join(TEST_DATA_DIR, 'iow-bus-gtfs.zip'))
---
+++ tests/test_utils.py
@@ -0,0 +1,33 @@
+import os
+
+import pandas as pd
+
+from gtfs_skims import utils
+
+
+def test_parse_timestamp():
+    assert utils.ts_to_sec('00:00:00') == 0
+    assert utils.ts_to_sec('10:01:01') == 36061
+
+
+def test_get_logger(tmpdir):
+    logger = utils.get_logger(os.path.join(tmpdir, 'logs', 'log.log'))
+    logger.info('test')
+
+
+def test_load_config(config):
+    'path_gtfs' in config.__dict__
+
+
+def test_load_gtfs(gtfs_data):
+    for x in ['calendar', 'routes', 'stops', 'stop_times', 'trips']:
+        assert isinstance(getattr(gtfs_data, x), pd.DataFrame)
+
+
+def test_cache_gtfs(gtfs_data, tmpdir):
+    gtfs_data.save(tmpdir)
+    gtfs_cached = utils.GTFSData.from_parquet(tmpdir)
+    for x in ['calendar', 'routes', 'stops', 'stop_times', 'trips']:
+        pd.testing.assert_frame_equal(
+            getattr(gtfs_data, x), getattr(gtfs_cached, x)
+        )
