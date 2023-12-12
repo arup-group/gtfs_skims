18410ab8f89a008d8d02f28cffb0de05c736ec72
Theodore.Chatziioannou <Theodore.Chatziioannou@arup.com>
2023-12-12 14:14:33 +0000

    shortest distances

--- gtfs_skims/graph.py
+++ gtfs_skims/graph.py
@@ -1,4 +1,6 @@
-import os
+from functools import partial
+import multiprocessing
+from typing import Optional
 
 from graph_tool import Graph
 from graph_tool.topology import shortest_distance
@@ -49,3 +51,89 @@ def get_all_edges(gtfs_data: GTFSData, connectors_data: ConnectorsData) -> pd.Da
     ], axis=0).fillna(0).map(int)
 
     return edges
+
+
+def add_gc(edges: pd.DataFrame, config: Config) -> pd.DataFrame:
+    """Calculate generalised time and add it as a column to the 'edges' table.
+
+    Args:
+        edges (pd.DataFrame): Edges dataframe. Should include these columns: 
+            ['ivt', 'walk', 'wait', 'transfer']
+        config (Config): Config object.
+
+    Returns:
+        pd.DataFrame: Edges dataframe, with the generalised time ("gc") column included.
+    """
+    edges['gc'] = edges['ivt'] +\
+        edges['walk'] * config.weight_walk +\
+        edges['wait'] * config.weight_wait +\
+        edges['transfer'] * config.penalty_interchange
+
+    # adding unweighted time as well
+    edges['time'] = edges[['ivt', 'walk', 'wait']].sum(1)
+
+    return edges['gc']
+
+
+def build_graph(
+        edges: pd.DataFrame,
+        vars=['ivt', 'walk', 'wait', 'time', 'gc']
+) -> Graph:
+    """Build a network graph from the edges table.
+
+    Args:
+        edges (pd.DataFrame): Edges table. Should include the 'gc' and 'time' columns from the 'add_gc' method.
+        vars (list): list of variables to include in the graph as edge properties.
+
+    Returns:
+        Graph: Connected GTFS graph
+    """
+    eprops = [(x, 'int') for x in vars]
+    g = Graph(
+        edges[['onode', 'dnode']+vars].values,
+        hashed=False,
+        eprops=eprops
+    )
+    return g
+
+
+def get_shortest_distances_single(
+        graph: Graph,
+        onode: int,
+        dnodes: list[int],
+        max_dist: Optional[float] = None,
+        attribute: str = 'gc'
+) -> np.ndarray:
+    d = shortest_distance(graph, onode, dnodes,
+                          weights=graph.edge_properties[attribute], dense=False,
+                          max_dist=max_dist, directed=True)
+    d = np.concatenate([np.array([onode]), d])
+
+    return d
+
+
+def get_shortest_distances(
+        graph: Graph,
+        onodes: list[int],
+        dnodes: list[int],
+        max_dist: Optional[float] = None,
+        attribute: str = 'gc'
+) -> pd.DataFrame:
+    n_cpus = multiprocessing.cpu_count() - 1
+    dist_wrapper = partial(get_shortest_distances_single, graph, dnodes=dnodes,
+                           max_dist=max_dist, attribute=attribute)
+    with multiprocessing.Pool(n_cpus) as pool_obj:
+        dists = pool_obj.map(dist_wrapper, onodes)
+
+    dists = np.array(dists)
+    dists = dists[dists[:, 0].argsort()]  # sort by source node
+
+    # convert to dataframe and reindex
+    dists = pd.DataFrame(dists[:, 1:], index=dists[:, 0], columns=dnodes)
+    dists = dists.loc[onodes]
+
+    return dists
+
+
+def main():
+    pass
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,10 +1,41 @@
 import os
 
+from graph_tool import Graph
 import numpy as np
 import pandas as pd
 import pytest
+from unittest.mock import Mock
 
 from gtfs_skims import graph
+
+
+@pytest.fixture()
+def mock_config(mocker):
+    mock = Mock()
+    mock.weight_wait = 3
+    mock.weight_walk = 2
+    mock.penalty_interchange = 600
+    return mock
+
+
+@pytest.fixture()
+def small_graph() -> Graph:
+    edges = pd.DataFrame({
+        'onode': [0, 0, 1, 2],
+        'dnode': [1, 2, 3, 3],
+        'gc': [10, 20, 15, 4]
+    })
+    return graph.build_graph(edges, vars=['gc'])
+
+
+@pytest.fixture()
+def small_graph_birectional() -> Graph:
+    edges = pd.DataFrame({
+        'onode': [0, 0, 1, 2, 1, 2, 3, 3],
+        'dnode': [1, 2, 3, 3, 0, 0, 1, 2],
+        'gc': [10, 20, 15, 4, 10, 20, 15, 4]
+    })
+    return graph.build_graph(edges, vars=['gc'])
 
 
 def test_get_ivt_times():
@@ -23,11 +54,46 @@ def test_get_ivt_times():
 def test_get_all_edges(gtfs_data_preprocessed, connectors_data):
     edges = graph.get_all_edges(gtfs_data_preprocessed, connectors_data)
 
-    len_expected = len(gtfs_data_preprocessed.stop_times) - gtfs_data_preprocessed.stop_times['trip_id'].nunique()
+    # all connections are included
+    len_expected = len(gtfs_data_preprocessed.stop_times) - \
+        gtfs_data_preprocessed.stop_times['trip_id'].nunique()
     len_expected += len(connectors_data.connectors_transfer)
     len_expected += len(connectors_data.connectors_access)
     len_expected += len(connectors_data.connectors_egress)
     assert len(edges) == len_expected
 
+    # all variables are included
     assert list(edges.columns) == [
         'onode', 'dnode', 'ivt', 'walk', 'wait', 'transfer']
+
+
+def test_calculate_gc(mock_config):
+    edges = pd.DataFrame({
+        'ivt': [100, 200],
+        'walk': [30, 10],
+        'wait': [10, 5],
+        'transfer': [0, 1]
+    })
+    graph.add_gc(edges, mock_config)
+    assert list(edges['gc']) == [190, 835]
+
+
+def test_get_shortest_distance_single(small_graph):
+    dists = graph.get_shortest_distances_single(small_graph, 0, [3, 2, 1, 0])
+    expected = np.array([24, 20, 10, 0])
+    assert dists[0] == 0  # the first value is the source
+    np.testing.assert_equal(dists[1:], expected)
+
+
+def test_get_distance_matrix(small_graph_birectional):
+    distmat = graph.get_shortest_distances(
+        small_graph_birectional, [0, 1, 2], [1, 2])
+    expected = np.array([
+        [10, 20],
+        [0, 19],
+        [19, 0]
+    ])
+    assert list(distmat.index) == [0, 1, 2]
+    assert list(distmat.columns) == [1, 2]
+
+    np.testing.assert_equal(distmat.values, expected)
