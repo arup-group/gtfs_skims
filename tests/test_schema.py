e043c575efc1c528053d4a633fd8dfb4cbcf8e1e
Theodore.Chatziioannou <Theodore.Chatziioannou@arup.com>
2024-01-08 17:43:41 +0000

    add schema

---
+++ tests/test_schema.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+import yaml
+
+import jsonschema
+
+CONFIG_DATA_DIR = os.path.join(Path(__file__).parent, "..", "config")
+TEST_DATA_DIR = os.path.join(Path(__file__).parent, "test_data")
+
+
+def test_config_schema(config):
+    with open(os.path.join(CONFIG_DATA_DIR, "schema.yaml"), "r") as f:
+        schema = yaml.safe_load(f)
+
+    with open(os.path.join(TEST_DATA_DIR, "config_demo.yaml")) as f:
+        config = yaml.safe_load(f)
+
+    jsonschema.validate(config, schema)
