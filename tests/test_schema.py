23fbff0f4f437cf9e08f1931eae8eb7d7630156e
Theodore.Chatziioannou <Theodore.Chatziioannou@arup.com>
2024-01-09 16:35:57 +0000

    comments

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,18 +1,21 @@
+import importlib
 import os
 from pathlib import Path
 
 import jsonschema
 import yaml
 
-CONFIG_DATA_DIR = os.path.join(Path(__file__).parent, "..", "config")
+from gtfs_skims import config as schema_dir
+
 TEST_DATA_DIR = os.path.join(Path(__file__).parent, "test_data")
 
 
-def test_config_schema(config):
-    with open(os.path.join(CONFIG_DATA_DIR, "schema.yaml"), "r") as f:
+def test_config_schema():
+    path = importlib.resources.files(schema_dir) / "schema.yaml"
+    with open(path, "r") as f:
         schema = yaml.safe_load(f)
 
     with open(os.path.join(TEST_DATA_DIR, "config_demo.yaml")) as f:
         config = yaml.safe_load(f)
 
-    jsonschema.validate(config, schema)
+    jsonschema.validate(config, schema, cls=jsonschema.Draft202012Validator)
